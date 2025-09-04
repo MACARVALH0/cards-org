2c80aaf8e41cd1a5e4097a95e88ab94644aedf5e
MACARVALH0 <macarvalho.dev@gmail.com>
2025-09-04 14:24:01 -0300

    Indexação alterada.

--- public/js/deck-global.js
+++ public/js/deck-global.js
@@ -1,19 +1,22 @@
-const checklist_DOM = document.querySelector("#checklist");
-const checklist_items = checklist_DOM.querySelector("#checklist-items");
+export default DOM =
+{
+    checklist_DOM: document.querySelector("#checklist"),
+    checklist_items: checklist_DOM.querySelector("#checklist-items"),
 
-const checklist_new_item = checklist_items.querySelector(".task");
-const new_item_input = checklist_new_item.querySelector("input");
-const task_placeholder_txt = new_item_input.value;
+    checklist_new_item: checklist_items.querySelector(".task"),
+    new_item_input: checklist_new_item.querySelector("input"),
+    task_placeholder_txt: new_item_input.value,
 
-const bio_DOM = document.querySelector("#bio");
+    bio_DOM: document.querySelector("#bio"),
 
-const journal_DOM = document.querySelector("#journal");
-const journal_entries = document.querySelectorAll(".journal-min-entry");
-const journal_modal = document.querySelector("#journal-modal");
-const journal_modal_box = journal_modal.querySelector("#journal-modal-box");
-const journal_modal_entry_delete_btn = journal_modal_box.querySelector("#journal-modal-entry-delete");
-const journal_modal_title = journal_modal_box.querySelector("#journal-modal-title input");
-const journal_modal_body = journal_modal_box.querySelector("#journal-modal-body textarea");
+    journal_DOM: document.querySelector("#journal"),
+    journal_entries: document.querySelectorAll(".journal-min-entry"),
+    journal_modal: document.querySelector("#journal-modal"),
+    journal_modal_box: journal_modal.querySelector("#journal-modal-box"),
+    journal_modal_entry_delete_btn: journal_modal_box.querySelector("#journal-modal-entry-delete"),
+    journal_modal_title: journal_modal_box.querySelector("#journal-modal-title input"),
+    journal_modal_body: journal_modal_box.querySelector("#journal-modal-body textarea"),
 
 
-var last_entry_element = null;
+    last_entry_element: null
+}
