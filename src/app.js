f76c85b9a0b03970585812aaba4b1e1ee4d1aa4f
MACARVALH0 <macarvalho.dev@gmail.com>
2025-09-04 14:27:47 -0300

    Documentação aprimorada.

--- src/app.js
+++ src/app.js
@@ -1,23 +1,33 @@
 const express = require('express');
-const ejs = require('ejs');
+// const ejs = require('ejs');
 const routes = require('./routes.js');
 
 const app = express();
-const server_port = 3000;
+const SERVER_PORT = 3000;
 
+// Define o uso de JSON como parsing único.
 app.use(express.json());
+
+// Utiliza o diretório `public/` para servir arquivos estáticos. 
 app.use(express.static('public'));
+
+// Define uma rota principal `/api` para as requisições à API.
 app.use('/api', routes);
+
+// Define a View Engine como EJS para renderização da página no servidor.
 app.set("view engine", "ejs");
 
-app.listen(server_port, () =>
+
+// Prepara a porta descrita em `SERVER_PORT`.
+app.listen(SERVER_PORT, () =>
 { 
     app._router.stack.forEach((middleware) =>
     {
         if (middleware.route){ console.log(middleware.route.path, middleware.route.methods); }
 
         else if (middleware.name === 'router')
-        { // Se for um router, verifica as sub-rotas
+        {
+            // Se for um router, verifica as sub-rotas.
             middleware.handle.stack.forEach((handler) =>
             {
                 const route = handler.route;
@@ -26,7 +36,7 @@ app.listen(server_port, () =>
         }
     });
 
-    console.log(`\nServidor ativo na porta ${server_port}.`);
+    console.log(`\nServidor ativo na porta ${SERVER_PORT}.`);
 
 
 });
